8279dbea836d3e0cbc730f2463e9417888897040
fd <fndercole@gmail.com>
2016-07-26 19:57:47 -0300

    Initial commit

---
+++ spec/main-spec.js
@@ -0,0 +1,11 @@
+'use babel'
+
+describe('The spell-simple provider for Atom Linter', () => {
+  const grammar = require('../lib/main').provideGrammar()[0]
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('spell-simple')
+    })
+  })
+})
