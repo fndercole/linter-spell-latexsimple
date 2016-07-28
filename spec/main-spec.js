9d125accf7fb0cfb7612ead5f6f50264e30c7fa6
fd <fndercole@gmail.com>
2016-07-28 17:06:51 -0300

    Change name of package

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,11 +1,11 @@
 'use babel'
 
-describe('The spell-simple provider for Atom Linter', () => {
+describe('The linter-spell-latexsimple provider for Atom Linter', () => {
   const grammar = require('../lib/main').provideGrammar()[0]
 
   beforeEach(() => {
     waitsForPromise(() => {
-      return atom.packages.activatePackage('spell-simple')
+      return atom.packages.activatePackage('linter-spell-latexsimple')
     })
   })
 })
